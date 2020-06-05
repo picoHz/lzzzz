f4ecc59b0fa130f6ed41d1c457266f792a3e3653
Ron Hashimoto <mail@h2so5.net>
2020-06-05 17:21:04 +0900

    build

--- build.rs
+++ build.rs
@@ -1,19 +1,13 @@
-use cc::Build;
-use globwalk;
 use std::io::{Error, ErrorKind};
 
 fn main() -> Result<(), Error> {
-    let sources: Vec<_> = globwalk::glob("vendor/liblz4/lib/**/*.c")?
-        .filter_map(Result::ok)
-        .map(|entry| entry.into_path())
-        .collect();
-
-    for path in &sources {
-        println!("cargo:rerun-if-changed={}", path.display());
-    }
-
-    Build::new()
-        .files(sources)
+    cc::Build::new()
+        .files(
+            globwalk::glob("vendor/liblz4/lib/**/*.c")?
+                .filter_map(Result::ok)
+                .map(|entry| entry.into_path())
+                .inspect(|path| println!("cargo:rerun-if-changed={}", path.display())),
+        )
         .try_compile("lz4")
         .map_err(|err| Error::new(ErrorKind::Other, err))
 }
