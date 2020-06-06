7b37cbc2054db5a867e034095a4cabafa09661ea
Ron Hashimoto <mail@h2so5.net>
2020-06-07 00:19:44 +0900

    state

--- src/lz4/api.rs
+++ src/lz4/api.rs
@@ -27,6 +27,28 @@ pub fn compress_fast(src: &[u8], dst: &mut [u8], acceleration: i32) -> usize {
     }
 }
 
+pub fn size_of_state() -> usize {
+    unsafe { binding::LZ4_sizeofState() as usize }
+}
+
+pub fn compress_fast_ext_state(
+    state: &mut [u8],
+    src: &[u8],
+    dst: &mut [u8],
+    acceleration: i32,
+) -> usize {
+    unsafe {
+        binding::LZ4_compress_fast_extState(
+            state.as_mut_ptr() as *mut c_void,
+            src.as_ptr() as *const c_char,
+            dst.as_mut_ptr() as *mut c_char,
+            src.len() as c_int,
+            dst.len() as c_int,
+            acceleration as c_int,
+        ) as usize
+    }
+}
+
 pub fn decompress_safe(src: &[u8], dst: &mut [u8]) -> i32 {
     unsafe {
         binding::LZ4_decompress_safe(
