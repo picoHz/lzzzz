7ddf72a59d48bc64755866134106426d32ce6b0a
Ron Hashimoto <mail@h2so5.net>
2020-06-07 00:52:23 +0900

    compress

--- src/lz4/mod.rs
+++ src/lz4/mod.rs
@@ -1,2 +1,33 @@
 mod api;
 mod binding;
+
+use crate::{LZ4Error, Result};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum CompressionMode {
+    Default,
+    Fast(i32),
+    FastExtState(i32),
+}
+
+impl Default for CompressionMode {
+    fn default() -> Self {
+        Self::Default
+    }
+}
+
+pub fn compress(src: &[u8], dst: &mut [u8], mode: CompressionMode) -> Result<usize> {
+    let len = match mode {
+        CompressionMode::Default => api::compress_default(src, dst),
+        CompressionMode::Fast(acc) => api::compress_fast(src, dst, acc),
+        CompressionMode::FastExtState(acc) => {
+            let mut state = vec![0; api::size_of_state()];
+            api::compress_fast_ext_state(&mut state, src, dst, acc)
+        }
+    };
+    if len > 0 {
+        Ok(len)
+    } else {
+        Err(LZ4Error::from("Failed"))
+    }
+}
