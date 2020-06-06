122e9d6709ed883f046d4eb9b3c7651176e65cd0
Ron Hashimoto <mail@h2so5.net>
2020-06-07 00:07:23 +0900

    gt

--- src/lz4/api.rs
+++ src/lz4/api.rs
@@ -1,1 +1,39 @@
+#![allow(unsafe_code)]
 
+use super::binding;
+
+use libc::{c_char, c_int, c_uint, c_void, size_t};
+
+pub fn compress_default(src: &[u8], dst: &mut [u8]) -> usize {
+    unsafe {
+        binding::LZ4_compress_default(
+            src.as_ptr() as *const c_char,
+            dst.as_mut_ptr() as *mut c_char,
+            src.len() as c_int,
+            dst.len() as c_int,
+        ) as usize
+    }
+}
+
+pub fn compress_fast(src: &[u8], dst: &mut [u8], acceleration: i32) -> usize {
+    unsafe {
+        binding::LZ4_compress_fast(
+            src.as_ptr() as *const c_char,
+            dst.as_mut_ptr() as *mut c_char,
+            src.len() as c_int,
+            dst.len() as c_int,
+            acceleration as c_int,
+        ) as usize
+    }
+}
+
+pub fn decompress_safe(src: &[u8], dst: &mut [u8]) -> i32 {
+    unsafe {
+        binding::LZ4_decompress_safe(
+            src.as_ptr() as *const c_char,
+            dst.as_mut_ptr() as *mut c_char,
+            src.len() as c_int,
+            dst.len() as c_int,
+        ) as i32
+    }
+}
--- src/lz4hc/api.rs
+++ src/lz4hc/api.rs
@@ -1,1 +1,5 @@
+#![allow(unsafe_code)]
 
+use super::binding;
+
+use libc::{c_char, c_int, c_uint, c_void, size_t};
