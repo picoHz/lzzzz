a25436edfa7e22f58789d50033902ff3ed6261d4
Ron Hashimoto <mail@h2so5.net>
2020-06-07 01:01:57 +0900

    t

--- src/lz4hc/mod.rs
+++ src/lz4hc/mod.rs
@@ -1,2 +1,35 @@
 mod api;
 mod binding;
+
+use crate::Result;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum CompressionMode {
+    Default,
+    ExtState,
+    DestSize,
+}
+
+impl Default for CompressionMode {
+    fn default() -> Self {
+        Self::Default
+    }
+}
+
+/// Compression level.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum CompressionLevel {
+    Custom(i32),
+    Default,
+    High,
+    Max,
+}
+
+pub fn compress(
+    src: &[u8],
+    dst: &mut [u8],
+    mode: CompressionMode,
+    compression_level: CompressionLevel,
+) -> Result<usize> {
+    todo!();
+}
