fb7b1870aaacf7537797676c4553db8afcd35641
Ron Hashimoto <mail@h2so5.net>
2020-06-06 23:47:09 +0900

    hc

--- src/lz4hc/binding.rs
+++ src/lz4hc/binding.rs
@@ -16,6 +16,32 @@ pub struct LZ4StreamHC {
 
 #[link(name = "lz4")]
 extern "C" {
+    pub fn LZ4_compress_HC(
+        src: *const c_char,
+        dst: *mut c_char,
+        src_size: c_int,
+        dst_capacity: c_int,
+        compression_level: c_int,
+    ) -> c_int;
+
+    pub fn LZ4_sizeofStateHC() -> c_int;
+    pub fn LZ4_compress_HC_extStateHC(
+        state: *mut c_void,
+        src: *const c_char,
+        dst: *mut c_char,
+        src_size: c_int,
+        dst_capacity: c_int,
+        compression_level: c_int,
+    ) -> c_int;
+    pub fn LZ4_compress_HC_destSize(
+        state: *mut c_void,
+        src: *const c_char,
+        dst: *mut c_char,
+        src_size_ptr: *mut c_int,
+        target_dst_dize: c_int,
+        compression_level: c_int,
+    ) -> c_int;
+
     pub fn LZ4_createStreamHC() -> *mut LZ4StreamHC;
     pub fn LZ4_freeStreamHC(ptr: *mut LZ4StreamHC) -> c_int;
     pub fn LZ4_resetStreamHC_fast(ptr: *mut LZ4StreamHC, compression_level: c_int);
