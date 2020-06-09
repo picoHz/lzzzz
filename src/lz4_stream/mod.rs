50e6a8245320f151ea56a9dbde6dd3363232c94c
Ron Hashimoto <mail@h2so5.net>
2020-06-09 23:54:52 +0900

    x

--- src/lz4_stream/mod.rs
+++ src/lz4_stream/mod.rs
@@ -1,3 +1,15 @@
 //! LZ4 Streaming Compressor/Decompressor
 
 mod api;
+
+pub struct StreamCompressor<D> {
+    device: D,
+}
+
+impl<D> StreamCompressor<D> {}
+
+pub struct StreamDecompressor<D> {
+    device: D,
+}
+
+impl<D> StreamDecompressor<D> {}
