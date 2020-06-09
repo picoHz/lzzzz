2279374fd1af7c5421f617d9174a39cf26070094
Ron Hashimoto <mail@h2so5.net>
2020-06-09 15:04:49 +0900

    x

--- src/lz4f/mod.rs
+++ src/lz4f/mod.rs
@@ -465,6 +465,13 @@ impl<'a> Default for DecompressionMode<'a> {
     }
 }
 
+/// Resolve a dictinary data from a dictionary id.
+pub trait DictResolver<'a> {
+    fn resolve(dict_id: u32) -> Result<&'a [u8]> {
+        Ok(&[])
+    }
+}
+
 thread_local!(static DECOMPRESSION_CTX: RefCell<DecompressionContext> = 
     RefCell::new(DecompressionContext::new().unwrap()));
 
