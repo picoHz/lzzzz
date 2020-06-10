2be6a38617a27e98b0ef6a982b60b518b29de7fd
Ron Hashimoto <mail@h2so5.net>
2020-06-10 16:39:33 +0900

    x

--- src/lz4_stream/mod.rs
+++ src/lz4_stream/mod.rs
@@ -2,14 +2,68 @@
 
 mod api;
 
-pub struct StreamCompressor<D> {
-    device: D,
+use std::{
+    any::Any,
+    marker::PhantomData,
+    rc::{Rc, Weak},
+};
+
+pub struct Outlive<'a> {
+    owner: Weak<()>,
+    phantom: PhantomData<&'a ()>,
 }
 
-impl<D> StreamCompressor<D> {}
-
-pub struct StreamDecompressor<D> {
-    device: D,
+impl<'a> Outlive<'a> {
+    fn new(owner: Weak<()>) -> Self {
+        Self {
+            owner,
+            phantom: PhantomData,
+        }
+    }
 }
 
-impl<D> StreamDecompressor<D> {}
+pub struct StreamCompressor {
+    rc: Rc<()>,
+}
+
+impl StreamCompressor {
+    pub fn new() -> Self {
+        Self { rc: Rc::new(()) }
+    }
+
+    pub fn begin<'a, 'b>(&mut self, src: &'a [u8], dst: &'b mut [u8]) -> Outlive<'a> {
+        // Invalidate the current Outlive.
+        self.rc = Rc::new(());
+        Outlive::new(Rc::downgrade(&self.rc))
+    }
+
+    pub fn next<'a, 'b, 'c>(
+        &mut self,
+        prev: Outlive<'a>,
+        src: &'b [u8],
+        dst: &'c mut [u8],
+    ) -> Outlive<'b> {
+        if !prev.owner.ptr_eq(&Rc::downgrade(&self.rc)) {
+            panic!("aaaa");
+        }
+        Outlive::new(Rc::downgrade(&self.rc))
+    }
+
+    pub fn end<'a, 'b>(&mut self, prev: Outlive<'a>, dst: &'b mut [u8]) {
+        if !prev.owner.ptr_eq(&Rc::downgrade(&self.rc)) {
+            panic!("aaaa");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn compression_context() {
+        let mut a = super::StreamCompressor::new();
+        let c = vec![0, 4];
+        let r = { a.begin(&c, &mut []) };
+        let c = vec![0, 4];
+        let r = a.next(r, &c, &mut []);
+    }
+}
