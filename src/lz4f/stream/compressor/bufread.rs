42263310395b652f7ac625d88b0bd03dbb56f1fc
Ron Hashimoto <mail@h2so5.net>
2020-06-15 08:12:17 +0900

    bufread

--- src/lz4f/stream/compressor/bufread.rs
+++ src/lz4f/stream/compressor/bufread.rs
@@ -1,8 +1,23 @@
-use super::Compressor;
-use std::io::{BufRead, Read, Result};
+use super::{Compressor, CompressorBuilder, Dictionary, Preferences};
+use std::{
+    convert::TryInto,
+    io::{BufRead, Read, Result},
+};
 
 pub struct BufReadCompressor<B: BufRead> {
-    ctx: Compressor<B>,
+    inner: Compressor<B>,
+}
+
+impl<B: BufRead> BufReadCompressor<B> {
+    pub(crate) fn new(
+        bufreader: B,
+        pref: Preferences,
+        dict: Option<Dictionary>,
+    ) -> crate::Result<Self> {
+        Ok(Self {
+            inner: Compressor::new(bufreader, pref, None)?,
+        })
+    }
 }
 
 impl<B: BufRead> Read for BufReadCompressor<B> {
@@ -18,5 +33,12 @@ impl<B: BufRead> BufRead for BufReadCompressor<B> {
 
     fn consume(&mut self, amt: usize) {
         unimplemented!()
+    }
+}
+
+impl<B: BufRead> TryInto<BufReadCompressor<B>> for CompressorBuilder<B> {
+    type Error = crate::Error;
+    fn try_into(self) -> crate::Result<BufReadCompressor<B>> {
+        BufReadCompressor::new(self.device, self.pref, self.dict)
     }
 }
--- src/lz4f/stream/compressor/read.rs
+++ src/lz4f/stream/compressor/read.rs
@@ -1,3 +1,24 @@
-use std::io::Read;
+use super::{BufReadCompressor, CompressorBuilder};
+use std::{
+    convert::TryInto,
+    io::{BufReader, Read, Result},
+};
 
-pub struct ReadCompressor {}
+pub struct ReadCompressor<R: Read> {
+    bufread: BufReadCompressor<BufReader<R>>,
+}
+
+impl<R: Read> Read for ReadCompressor<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.bufread.read(buf)
+    }
+}
+
+impl<R: Read> TryInto<ReadCompressor<R>> for CompressorBuilder<R> {
+    type Error = crate::Error;
+    fn try_into(self) -> crate::Result<ReadCompressor<R>> {
+        Ok(ReadCompressor {
+            bufread: BufReadCompressor::new(BufReader::new(self.device), self.pref, self.dict)?,
+        })
+    }
+}
