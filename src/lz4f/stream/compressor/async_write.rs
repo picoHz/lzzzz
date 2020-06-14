f8d7b5c5ca2813904f60d94e29c6bbd7beeb2a66
Ron Hashimoto <mail@h2so5.net>
2020-06-14 23:26:59 +0900

    x

--- src/lz4f/stream/compressor/async_write.rs
+++ src/lz4f/stream/compressor/async_write.rs
@@ -1,6 +1,20 @@
 #![cfg(feature = "tokio-io")]
 
-use tokio::io::AsyncWrite;
+use super::{Compressor, CompressorBuilder, Dictionary, Preferences, State, LZ4F_HEADER_SIZE_MAX};
+use futures::future::FutureExt;
+use pin_project::{pin_project, project};
+use std::{
+    convert::TryInto,
+    marker::Unpin,
+    mem::MaybeUninit,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::io::{AsyncWrite, AsyncWriteExt, Result};
 
 #[cfg_attr(docsrs, doc(cfg(feature = "tokio-io")))]
-pub struct AsyncWriteCompressor {}
+#[pin_project]
+pub struct AsyncWriteCompressor<W: AsyncWrite> {
+    #[pin]
+    dev: W,
+}
