f93fc1dccbbcc3666b54eeef1cc22e1d5348c6c9
picoHz <picoHz@outlook.com>
2020-06-14 20:19:23 +0900

    x

--- src/lz4_hc/stream/mod.rs
+++ src/lz4_hc/stream/mod.rs
@@ -56,11 +56,15 @@ impl<'a> Compressor<'a> {
         })
     }
 
+    #[cfg(feature = "liblz4-experimental")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "liblz4-experimental")))]
     pub fn set_compression_level(&mut self, level: CompressionLevel) {
         self.compression_level = level;
         self.ctx.set_compression_level(level.as_i32());
     }
 
+    #[cfg(feature = "liblz4-experimental")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "liblz4-experimental")))]
     pub fn set_favor_dec_speed(&mut self, flag: bool) {
         self.ctx.set_favor_dec_speed(flag);
     }
