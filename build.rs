b8b8f319cd2913ee8b5900868b5ccc6b30ccfc13
Ron Hashimoto <mail@h2so5.net>
2020-06-18 00:41:08 +0900

    s

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), cc::Error> {
-    let files = &["lz4hc.c", "lz4frame.c", "xxhash.c", "lz4.c"][..];
+    let sources = &["lz4.c", "lz4hc.c", "lz4frame.c", "xxhash.c"][..];
     let dir = std::path::Path::new("./vendor/liblz4/lib/");
     cc::Build::new()
-        .files(files.iter().map(|file| dir.join(file)))
+        .files(sources.iter().map(|file| dir.join(file)))
         .try_compile("lz4")
 }
