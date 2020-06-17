92289d3c907ec16175e9cc794ce94fc7f5a8d7ce
picoHz <picoHz@outlook.com>
2020-06-17 18:22:53 +0900

    x

---
+++ src/lz4f/dictionary.rs
@@ -0,0 +1,26 @@
+use super::api::DictionaryHandle;
+use crate::Result;
+use std::sync::Arc;
+
+/// A pre-compiled dictionary for the efficient compression.
+///
+/// **Cited from lz4frame.h:**
+///
+/// A Dictionary is useful for the compression of small messages (KB range).
+/// It dramatically improves compression efficiency.
+///
+/// LZ4 can ingest any input as dictionary, though only the last 64 KB are
+/// useful. Best results are generally achieved by using Zstandard's Dictionary
+/// Builder to generate a high-quality dictionary from a set of samples.
+#[derive(Clone)]
+pub struct Dictionary(Arc<DictionaryHandle>);
+
+impl Dictionary {
+    pub fn new(data: &[u8]) -> Result<Self> {
+        DictionaryHandle::new(data).map(|dict| Self(Arc::new(dict)))
+    }
+
+    pub(crate) fn handle(&self) -> &DictionaryHandle {
+        &self.0
+    }
+}
