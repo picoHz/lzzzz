ba0add6ee71e89b942e44604e960a0ce8cb93b1a
picoHz <picoHz@outlook.com>
2020-06-18 21:59:10 +0900

    liblz4

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), cc::Error> {
     let sources = &["lz4.c", "lz4hc.c", "lz4frame.c", "xxhash.c"][..];
-    let dir = std::path::Path::new("./vendor/liblz4/lib/");
+    let dir = std::path::Path::new("vendor/liblz4");
     cc::Build::new()
         .files(sources.iter().map(|file| dir.join(file)))
         .try_compile("lz4")
