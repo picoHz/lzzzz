91f8072e63578cb9b51c1d9084eeb6beba56c69f
picoHz <picoHz@outlook.com>
2020-06-25 23:17:36 +0900

    x

--- tests/lz4f_comp.rs
+++ tests/lz4f_comp.rs
@@ -1,23 +1,13 @@
 use lzzzz::{
     lz4f,
     lz4f::{
-        AutoFlush, BlockSize, CompressionLevel, FavorDecSpeed, Preferences, PreferencesBuilder,
+        AutoFlush, BlockChecksum, BlockMode, BlockSize, CompressionLevel, ContentChecksum, Error,
+        ErrorKind, FavorDecSpeed, Preferences, PreferencesBuilder,
     },
 };
 use rand::{distributions::Standard, rngs::SmallRng, Rng, SeedableRng};
 use rayon::prelude::*;
-use std::i32;
-
-fn test_compress_to_vec(prefs: &Preferences) {
-    for src in generate_data() {
-        let mut comp_buf = Vec::new();
-        let mut decomp_buf = Vec::new();
-
-        lz4f::compress_to_vec(&src, &mut comp_buf, prefs).unwrap();
-        lz4f::decompress_to_vec(&comp_buf, &mut decomp_buf).unwrap();
-        assert_eq!(decomp_buf, src);
-    }
-}
+use std::{i32, u32};
 
 fn generate_data() -> impl Iterator<Item = Vec<u8>> {
     (0..20).map(|n| {
@@ -26,9 +16,8 @@ fn generate_data() -> impl Iterator<Item = Vec<u8>> {
     })
 }
 
-#[test]
-fn compress_to_vec() {
-    let prefs = &[
+fn preferences_set() -> Vec<Preferences> {
+    vec![
         PreferencesBuilder::new().build(),
         PreferencesBuilder::new()
             .block_size(BlockSize::Max64KB)
@@ -41,6 +30,16 @@ fn compress_to_vec() {
             .build(),
         PreferencesBuilder::new()
             .block_size(BlockSize::Max4MB)
+            .build(),
+        PreferencesBuilder::new()
+            .block_mode(BlockMode::Independent)
+            .build(),
+        PreferencesBuilder::new()
+            .content_checksum(ContentChecksum::Enabled)
+            .build(),
+        PreferencesBuilder::new().dict_id(u32::MAX).build(),
+        PreferencesBuilder::new()
+            .block_checksum(BlockChecksum::Enabled)
             .build(),
         PreferencesBuilder::new()
             .compression_level(CompressionLevel::High)
@@ -60,7 +59,68 @@ fn compress_to_vec() {
         PreferencesBuilder::new()
             .auto_flush(AutoFlush::Enabled)
             .build(),
-    ][..];
+    ]
+}
 
-    prefs.par_iter().for_each(|p| test_compress_to_vec(&p));
+#[test]
+fn compress_to_vec() {
+    preferences_set().par_iter().for_each(|prefs| {
+        for src in generate_data() {
+            let mut comp_buf = Vec::new();
+            let mut decomp_buf = Vec::new();
+
+            assert_eq!(
+                lz4f::compress_to_vec(&src, &mut comp_buf, prefs)
+                    .unwrap()
+                    .dst_len(),
+                comp_buf.len()
+            );
+            assert_eq!(
+                lz4f::decompress_to_vec(&comp_buf, &mut decomp_buf)
+                    .unwrap()
+                    .dst_len(),
+                decomp_buf.len()
+            );
+            assert_eq!(decomp_buf, src);
+        }
+    });
+}
+
+#[test]
+fn compress() {
+    preferences_set().par_iter().for_each(|prefs| {
+        for src in generate_data() {
+            let mut comp_buf = Vec::new();
+            let mut decomp_buf = Vec::new();
+
+            comp_buf.resize_with(
+                lz4f::max_compressed_size(src.len(), &prefs),
+                Default::default,
+            );
+            let len = lz4f::compress(&src, &mut comp_buf, prefs)
+                .unwrap()
+                .dst_len();
+            comp_buf.resize_with(len, Default::default);
+            assert_eq!(
+                lz4f::decompress_to_vec(&comp_buf, &mut decomp_buf)
+                    .unwrap()
+                    .dst_len(),
+                decomp_buf.len()
+            );
+            assert_eq!(decomp_buf, src);
+        }
+    });
+}
+
+#[test]
+fn compress_too_small_dst() {
+    preferences_set().par_iter().for_each(|prefs| {
+        for src in generate_data() {
+            let mut comp_buf = Vec::new();
+            assert_eq!(
+                lz4f::compress(&src, &mut comp_buf, prefs),
+                Err(Error::Lz4f(ErrorKind::DstMaxSizeTooSmall))
+            );
+        }
+    });
 }
