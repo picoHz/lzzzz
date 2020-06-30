70db9addc747ac97e688be282b345e2a5740dffd
picoHz <picoHz@outlook.com>
2020-06-30 23:35:54 +0900

    lz4

--- tests/lz4.rs
+++ tests/lz4.rs
@@ -1,5 +1,5 @@
 use futures::future::join_all;
-use lzzzz::{lz4, lz4f::*};
+use lzzzz::lz4;
 
 mod common;
 use common::lz4_test_set;
@@ -13,11 +13,16 @@ mod compress {
             lz4_test_set()
                 .map(|src| async move {
                     let mut comp_buf = vec![0; lz4::max_compressed_size(src.len())];
-                    //let mut decomp_buf = Vec::new();
+                    let mut decomp_buf = vec![0; src.len()];
 
                     let len = lz4::compress(&src, &mut comp_buf, lz4::CompressionMode::Default)
                         .unwrap()
                         .dst_len();
+                    comp_buf.resize(len, 0);
+
+                    lz4::decompress(&comp_buf, &mut decomp_buf, lz4::DecompressionMode::Default)
+                        .unwrap();
+                    assert_eq!(src, decomp_buf);
                 })
                 .map(|task| tokio::spawn(task)),
         )
