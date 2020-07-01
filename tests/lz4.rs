c776fa2c6130f44a68333ad7bd49177b5fed48b5
Ron Hashimoto <mail@h2so5.net>
2020-07-02 00:25:18 +0900

    partial

--- tests/lz4.rs
+++ tests/lz4.rs
@@ -34,16 +34,35 @@ mod compress_to_vec {
         lz4_test_set().par_bridge().for_each(|(src, mode)| {
             let mut comp_buf = Vec::new();
             let mut decomp_buf = vec![0; src.len()];
-            let len = lz4::compress_to_vec(&src, &mut comp_buf, mode)
-                .unwrap()
-                .dst_len();
-            lz4::decompress(
-                &comp_buf[..len],
-                &mut decomp_buf,
-                lz4::DecompressionMode::Default,
-            )
-            .unwrap();
+            lz4::compress_to_vec(&src, &mut comp_buf, mode).unwrap();
+            lz4::decompress(&comp_buf, &mut decomp_buf, lz4::DecompressionMode::Default).unwrap();
             assert_eq!(decomp_buf, src);
         });
+    }
+}
+
+mod decompress {
+    use super::*;
+
+    #[test]
+    fn partial() {
+        lz4_test_set()
+            .map(|(src, mode)| (0..20).map(move |n| (src.clone(), mode, 16 << n)))
+            .flatten()
+            .par_bridge()
+            .for_each(|(src, mode, len)| {
+                let mut comp_buf = Vec::new();
+                let mut decomp_buf = vec![0; len];
+                lz4::compress_to_vec(&src, &mut comp_buf, mode).unwrap();
+                lz4::decompress(
+                    &comp_buf,
+                    &mut decomp_buf,
+                    lz4::DecompressionMode::Partial {
+                        original_size: src.len(),
+                    },
+                )
+                .unwrap();
+                assert!(src.starts_with(&decomp_buf));
+            });
     }
 }
