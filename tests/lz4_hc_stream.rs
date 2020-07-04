aecca7e620bdc84baf05580624f92b203bb44dd9
picoHz <picoHz@outlook.com>
2020-07-04 20:29:34 +0900

    stream

--- tests/lz4_hc_stream.rs
+++ tests/lz4_hc_stream.rs
@@ -80,4 +80,42 @@ mod compressor {
                 }
             });
     }
+
+    #[test]
+    fn dynamic_adaptation() {
+        lz4_hc_stream_test_set()
+            .par_bridge()
+            .for_each(|(src_set, _)| {
+                let mut stream = lz4_hc::Compressor::new().unwrap();
+                let mut rng = SmallRng::seed_from_u64(0);
+                for src in src_set {
+                    // TODO
+                    if src.len() >= 4194304 {
+                        continue;
+                    }
+                    let mut comp_buf = vec![0; lz4::max_compressed_size(src.len())];
+                    let mut decomp_buf = vec![0; src.len()];
+
+                    stream.set_compression_level(lz4_hc::CompressionLevel::Custom(rng.gen()));
+                    stream.set_favor_dec_speed(rng.gen_bool(0.5));
+
+                    let len = stream
+                        .next(
+                            Vec::from(src.as_ref()),
+                            &mut comp_buf,
+                            lz4_hc::CompressionMode::Default,
+                        )
+                        .unwrap()
+                        .dst_len();
+
+                    lz4::decompress(
+                        &comp_buf[..len],
+                        &mut decomp_buf,
+                        lz4::DecompressionMode::Default,
+                    )
+                    .unwrap();
+                    assert_eq!(decomp_buf, src);
+                }
+            });
+    }
 }
