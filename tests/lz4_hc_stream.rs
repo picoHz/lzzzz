7995d04dc150da64298fa397f2016884d57d3432
picoHz <picoHz@outlook.com>
2020-07-12 21:06:27 +0900

    x

--- tests/lz4_hc_stream.rs
+++ tests/lz4_hc_stream.rs
@@ -15,20 +15,16 @@ mod compressor {
         lz4_hc_stream_test_set()
             .par_bridge()
             .for_each(|(src_set, level)| {
-                let mut stream = lz4_hc::Compressor::new().unwrap();
-                stream.set_compression_level(level);
+                let mut comp = lz4_hc::Compressor::new().unwrap();
+                let mut decomp = lz4::Decompressor::new().unwrap();
+                comp.set_compression_level(level);
                 for src in src_set {
-                    // TODO
-                    if src.len() >= 4194304 {
-                        continue;
-                    }
                     let mut comp_buf = vec![0; lz4::max_compressed_size(src.len())];
-                    let mut decomp_buf = vec![0; src.len()];
-
-                    let len = stream.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
-
-                    lz4::decompress(&comp_buf[..len], &mut decomp_buf).unwrap();
-                    assert_eq!(decomp_buf, src);
+                    let len = comp.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
+                    assert_eq!(
+                        decomp.next(&comp_buf[..len], src.len()).unwrap(),
+                        src.as_ref()
+                    );
                 }
             });
     }
@@ -42,19 +38,16 @@ mod compressor {
                     .sample_iter(Standard)
                     .take(64_000)
                     .collect::<Vec<_>>();
-                let mut stream = lz4_hc::Compressor::with_dict(&dict).unwrap();
-                stream.set_compression_level(level);
+                let mut comp = lz4_hc::Compressor::with_dict(&dict).unwrap();
+                let mut decomp = lz4::Decompressor::with_dict(&dict).unwrap();
+                comp.set_compression_level(level);
                 for src in src_set {
-                    // TODO
-                    if src.len() >= 4194304 {
-                        continue;
-                    }
                     let mut comp_buf = vec![0; lz4::max_compressed_size(src.len())];
-                    let len = stream.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
-
-                    let mut decomp_buf = vec![0; src.len()];
-                    lz4::decompress_with_dict(&comp_buf[..len], &mut decomp_buf, &dict).unwrap();
-                    assert_eq!(src, decomp_buf);
+                    let len = comp.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
+                    assert_eq!(
+                        decomp.next(&comp_buf[..len], src.len()).unwrap(),
+                        src.as_ref()
+                    );
                 }
             });
     }
@@ -64,27 +57,24 @@ mod compressor {
         lz4_hc_stream_test_set()
             .par_bridge()
             .for_each(|(src_set, _)| {
-                let mut stream = lz4_hc::Compressor::new().unwrap();
+                let mut comp = lz4_hc::Compressor::new().unwrap();
+                let mut decomp = lz4::Decompressor::new().unwrap();
                 let mut rng = SmallRng::seed_from_u64(0);
                 for src in src_set {
-                    // TODO
-                    if src.len() >= 4194304 {
-                        continue;
-                    }
                     let mut comp_buf = vec![0; lz4::max_compressed_size(src.len())];
-                    let mut decomp_buf = vec![0; src.len()];
 
-                    stream.set_compression_level(rng.gen());
-                    stream.set_favor_dec_speed(if rng.gen_bool(0.5) {
+                    comp.set_compression_level(rng.gen());
+                    comp.set_favor_dec_speed(if rng.gen_bool(0.5) {
                         lz4_hc::FavorDecSpeed::Enabled
                     } else {
                         lz4_hc::FavorDecSpeed::Disabled
                     });
 
-                    let len = stream.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
-
-                    lz4::decompress(&comp_buf[..len], &mut decomp_buf).unwrap();
-                    assert_eq!(decomp_buf, src);
+                    let len = comp.next(Vec::from(src.as_ref()), &mut comp_buf).unwrap();
+                    assert_eq!(
+                        decomp.next(&comp_buf[..len], src.len()).unwrap(),
+                        src.as_ref()
+                    );
                 }
             });
     }
